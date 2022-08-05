e09262c15e12622ff3c676fb19e0736703dc5f34
Veer1498 <singavarapuveerendra@gmail.com>
2022-08-05 11:43:54 +0530

    [Veer] Added : Optized the early code of Line Comparision

--- src/lineComp/LineComparision.java
+++ src/lineComp/LineComparision.java
@@ -3,10 +3,10 @@ package lineComp;
 import java.util.Scanner;
 
 public class LineComparision {
-	public static double length1,length2;
+	public static double length1,length2,length;
 	static int x1,y1,x2,y2,value;
 
-	static void coordinates() {
+	public static void coordinates() {
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Please Enter Coordinates of Line");
 		int x1 = sc.nextInt();
@@ -18,27 +18,28 @@ public class LineComparision {
 		System.out.println("The points x2= " +x2);
 		System.out.println("The points y1= " +y1);
 		System.out.println("The points y2= " +y2);
+		length =(int) Math.sqrt(Math.pow((x2-x1),2)+Math.pow((y2-y1),2));
 	}
 	
-	static void lineLength() {
+	public static void lineLength() {
 		coordinates();
-		length1 =(int) Math.sqrt(Math.pow((x2-x1),2)+Math.pow((y2-y1),2));
+		length1 =length;
 		coordinates();
-		length2 =(int) Math.sqrt(Math.pow((x2-x1),2)+Math.pow((y2-y1),2));
+		length2 =length;
 		System.out.println(" ");
 		System.out.println("The Length of Line1 is :"+length1);
-		System.out.println("The Length of Line1 is :"+length2);	
+		System.out.println("The Length of Line2 is :"+length2);	
 	}
 	
-	static void Comparing() {
+	public static void Comparing() {
 		
 		value = Double.compare(length1,length2);
 		if(value == 0)
 			System.out.println("The Lines are Equal");
 		else if(value >1)
-			System.out.println("Line 1 is Greater than Line 2");
+			System.out.println("Line 2 is Greater than Line 2");
 		else
-			System.out.println("Line 2 is Greater than Line 1");
+			System.out.println("Line 1 is Greater than Line 1");
 	}
 	
 	public static void main(String[] args) {
